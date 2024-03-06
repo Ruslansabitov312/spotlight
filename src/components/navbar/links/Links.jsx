257ee7b4adeae4de18f054688cb3991413d402ba
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2024-03-06 23:40:37 +0600

    Add media queries for navbar and container

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,44 +1,59 @@
+"use client"
+
 import cls from './Links.module.css'
 import NavLink from "@/components/navbar/links/NavLink/NavLink";
+import {useState} from "react";
+
+const links = [
+    {
+        title: "Homepage",
+        path: '/',
+    },
+    {
+        title: "About",
+        path: '/about',
+    },
+    {
+        title: "Contact",
+        path: '/contact',
+    },
+    {
+        title: "Blog",
+        path: '/blog',
+    },
+]
 
 const Links = () => {
-    const links = [
-        {
-            title: "Homepage",
-            path: '/',
-        },
-        {
-            title: "About",
-            path: '/about',
-        },
-        {
-            title: "Contact",
-            path: '/contact',
-        },
-        {
-            title: "Blog",
-            path: '/blog',
-        },
-    ]
+    const [open, setOpen] = useState(false)
 
     // temporary
     const session = true
     const isAdmin = true
 
     return (
-        <div className={cls.links}>
-            {links.map((link) => (
-                <NavLink item={link} key={link.path} />
-            ))}
+        <div className={cls.container}>
+            <div className={cls.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.path} />
+                ))}
+                {
+                    session ? (
+                        <>
+                            {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
+                            <button className={cls.logoutBtn}>Logout</button>
+                        </>
+                    ) : (
+                        <NavLink item={{title: "Login", path: "/login"}} />
+                    )
+                }
+            </div>
+            <button className={cls.menuBtn} onClick={() => setOpen((prev) => !prev)}>menu</button>
             {
-                session ? (
-                    <>
-                        {isAdmin && <NavLink item={{title: "Admin", path: "/admin"}} />}
-                        <button className={cls.logoutBtn}>Logout</button>
-                    </>
-                ) : (
-                    <NavLink item={{title: "Login", path: "/login"}} />
-                )
+                open && <div className={cls.mobileLinks}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.path} />
+                    ))}
+                </div>
             }
         </div>
     )
