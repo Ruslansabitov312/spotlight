b9578b565ba8184e54e062a6eb124609e95a929f
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2024-03-07 00:10:19 +0600

    Get started with nextjs image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,13 @@
+import Image from "next/image";
+import cls from './about.module.css'
+
 const AboutPage = () => {
     return (
-        <div>About Page</div>
+        <div>
+            <div className={cls.imgContainer}>
+                <Image src={"/about.png"} alt={""} fill />
+            </div>
+        </div>
     )
 }
 
