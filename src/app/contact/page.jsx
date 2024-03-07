3bec5be88d0cc0d1d451a2ff905118f74c076b82
Sabitov Ruslan Almazovich <78593451+Ruslansabitov312@users.noreply.github.com>
2024-03-08 00:46:30 +0600

    Add contactPage screen

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,31 @@
+import cls from './Contact.module.css'
+import Image from "next/image";
+
 const ContactPage = () => {
     return (
-        <div>Contact Page</div>
+        <div className={cls.container}>
+            <div className={cls.imgContainer}>
+                <Image src={"/contact.png"} alt={""} className={cls.img} fill />
+            </div>
+            <div className={cls.formContainer}>
+                <form
+                    action=""
+                    className={cls.form}
+                >
+                    <input type="text" placeholder={"Name and Surname"} />
+                    <input type="text" placeholder={"Email Address"} />
+                    <input type="text" placeholder={"Phone Number (optional)"} />
+                    <textarea
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="10"
+                        placeholder={"message"}
+                    ></textarea>
+                    <button type={"button"} className={cls.submitBtn}>Send</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
